4e1ac754fe11f5d49ee436286858cd493e27ea79
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2025-01-15 10:03:59 +0100

    add new api to completion

--- shell-completions/hibp.fish
+++ shell-completions/hibp.fish
@@ -1,3 +1,3 @@
 complete -c hibp -n "__fish_use_subcommand" -f -a '(hibp commands)' -d 'subcommand'
 
-complete -c hibp -n "contains request (commandline -poc)" -f -a "breachedaccount breacheddomain subscribeddomains breaches breach latestbreach dataclasses pasteaccount subscription" -d "request"
+complete -c hibp -n "contains request (commandline -poc)" -f -a "breachedaccount breacheddomain subscribeddomains breaches breach latestbreach dataclasses pasteaccount subscription stealerlogsbyemail" -d "request"
